926cbc66117f2783358697ab4e3fe08a3beac40d
kang <hust.wangkang@gmail.com>
2025-02-05 23:36:12 +0800

    修改gitignore

--- Hera/include/Hera/HeraInit.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-namespace Hera {
-bool Init();
-
-}  // namespace Hera
--- Hera/msvc-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Hera2.0/Hera
-# Build directory: D:/Hera2.0/HeraBinaries/Hera/msvc-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("test")
