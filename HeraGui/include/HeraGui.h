2fc01cab2ca79e62c572662e79130787a0c77136
kang <hust.wangkang@gmail.com>
2025-02-09 14:30:02 +0800

    加入release模式的支持

--- HeraGui/include/HeraGui.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "HeraGui/Core/Window.h"
-#include "HeraGui/Core/LayerStack.h"
-#include "HeraGui/Application.h"
-#include "HeraGui/imgui/ImGuiLayer.h"
-// #include "HeraGui/EntryPoint.h"
---
+++ concurrencpp.cmake
@@ -0,0 +1,17 @@
+
+
+
+#${DEPENDENCY_DIR}/googletest 就是googletest源码所在文件夹
+set(SOURCE_DIR ${DEPENDENCY_DIR}/concurrencpp)
+
+
+#直接一个命令，全部搞定，因为它默认就是会开启install的，所以这里我们只需要设置install的prefix就可以了
+ExternalProject_Add(
+    concurrencpp_project
+    SOURCE_DIR ${SOURCE_DIR}
+    CMAKE_ARGS  -DCMAKE_INSTALL_PREFIX=${Binary_DIR}/concurrencpp/${LIB_TYPE_DIR_NAME}
+                -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+                -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+    INSTALL_DIR ${Binary_DIR}/concurrencpp/${LIB_TYPE_DIR_NAME}
+)
+
